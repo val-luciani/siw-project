c0776ec32a3179b63b834515ceb5a5fd622fcc09
Valerio Luciani <val.luciani@outlook.it>
2017-06-12 22:45:11 +0200

    Sistemato il Login (ora serve il Logout)
    Credenziali ADMIN: username="admin", password="admin"

--- src/main/java/it/uniroma3/siw/security/SecurityConfig.java
+++ src/main/java/it/uniroma3/siw/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.security;
- 
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,19 +9,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+          .withUser("admin").password("admin").roles("ADMIN")
+          .and()
+          .withUser("user").password("user").roles("USER");
+    }
  
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
-        .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-        .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-        .logout()
-           .permitAll();
+          .authorizeRequests()
+          .antMatchers("/login*").permitAll()
+          .anyRequest().hasRole("ADMIN")
+          .and()
+          .formLogin()
+          .loginPage("/login.html")
+          .loginProcessingUrl("/login")
+          .defaultSuccessUrl("/index.html")
+          .failureUrl("/login.html?error=true")
+          .and()
+          .logout().logoutSuccessUrl("/login.html");
     }
 }
