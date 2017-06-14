7a0be6473d0744fb9ad402fa4aa574a2f126433a
Valerio Luciani <Valerio Luciani@LAPTOP-JFN4TJ5L>
2017-06-14 10:06:41 +0200

    Inserita etichetta utente loggato
    index spostata in /templates in quanto usa funzionalità di Thymeleaf (e
    quindi creato "HomeController", altrimenti non la visualizza)

---
+++ src/main/java/it/uniroma3/siw/controller/HomeController.java
@@ -0,0 +1,16 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// controller to access the login page
+@Controller
+public class HomeController {
+
+  // Login form
+  @RequestMapping("/index.html")
+  public String admin() {
+    return "index";
+  }
+
+}
--- src/main/java/it/uniroma3/siw/security/SecurityConfig.java
+++ src/main/java/it/uniroma3/siw/security/SecurityConfig.java
@@ -1,12 +1,10 @@
 package it.uniroma3.siw.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebSecurity
