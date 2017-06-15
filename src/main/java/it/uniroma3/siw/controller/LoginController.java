ffb2279a8ceb7048ead8fe88dfbf94e5b816b9f9
Valerio Luciani <Valerio Luciani@LAPTOP-JFN4TJ5L>
2017-06-15 14:27:35 +0200

    Creato doc "specifiche" (da completare)

--- src/main/java/it/uniroma3/siw/controller/LoginController.java
+++ src/main/java/it/uniroma3/siw/controller/LoginController.java
@@ -1,7 +1,6 @@
 package it.uniroma3.siw.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // controller to access the login page
